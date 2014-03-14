ac303331687d53de55343afb50c6ed7646d6b3f4
Carlos Almada <car_092@hotmail.com>
2014-03-14 00:46:39 -0700

    Tile class

---
+++ Totemoc_sol/Totemoc/Tile.cpp
@@ -0,0 +1,29 @@
+#include "Tile.hpp"
+
+Tile::Tile(int type){
+	switch (type){
+		case 0:
+			mColor = sf::Color::Green;
+			break;
+		case 1:
+			mColor = sf::Color::Red;
+			break;
+		case 2:
+			mColor = sf::Color::Yellow;
+			break;
+		case 3:
+			mColor = sf::Color::Black;
+			break;
+		default:
+			mColor = sf::Color::Cyan;
+	}
+	mShape.setFillColor(mColor);
+}
+
+void Tile::draw(sf::RenderWindow& window, int tileX, int tileY, int camX, int camY){
+	sf::Vector2f screenPos;
+	screenPos.x = tileX - camX;
+	screenPos.y = tileY - camY;
+	mShape.setPosition(screenPos);
+	window.draw(mShape);
+}
---
+++ Totemoc_sol/Totemoc/Tile.hpp
@@ -0,0 +1,15 @@
+#ifndef TOTEMOC_TILE_HPP
+#define TOTEMOC_TILE_HPP
+
+#include "SFML/Graphics.hpp"
+
+class Tile{
+	private:
+		sf::Color mColor;
+		sf::RectangleShape mShape;
+	public:
+		Tile(int type);
+		void draw(sf::RenderWindow& window, int tileX, int tileY, int camX, int camY);
+};
+
+#endif //TOTEMOC_TILE_HPP
